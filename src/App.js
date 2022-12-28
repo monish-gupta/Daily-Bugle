ae3503bf1e6d7d01042acc55039ad1ba77a564e8
MONISH GUPTA <monishgupta72@gmail.com>
2022-12-28 14:55:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./header-logo.png";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "@mui/material";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://newsapi.org/v2/everything?q=technology&sources=bbc-news&apiKey=3a4057d1f5414f5ca8e2fc125065108a"
+      )
+      .then((res) => {
+        setArticles(res.data.articles.slice(0, 40));
+        // console.log(res.data.articles.slice(0, 20));
+        // content.forEach((article) => {
+        //   if (article.urlToImage != null) {
+        //     setArticles((prevarticles) => [...prevarticles, article]);
+        //   }
+        // });
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="article-box">
+        {articles.map((article, index) => {
+          const content = article.content.split("[+");
+          return (
+            <div key={index} className="article">
+              <img
+                src={article.urlToImage}
+                className="article-picture"
+                alt="News Detail"
+              />
+              <div>
+                <h1
+                  className="article-title"
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                >
+                  {article.title}
+                </h1>
+                <p className="article-content">{content[0]}</p>
+              </div>
+              <div>
+                <a
+                  href="{article.content}"
+                  target={"_blank"}
+                  className="article-link"
+                >
+                  Learn More
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
